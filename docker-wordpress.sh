3ba4c014dfef06aa050bae17ac322b3c4b9e51c9
Max Wittmann <damxam@gmail.com>
2018-04-01 23:02:52 -0700

    Works

--- docker-wordpress.sh
+++ docker-wordpress.sh
@@ -2,7 +2,9 @@ docker run --name $1_wordpress --network backend-net \
   -e WORDPRESS_DB_HOST=mysql \
   -e WORDPRESS_DB_USER=root \
   -e WORDPRESS_DB_PASSWORD=password \
+  -e WORDPRESS_DB_NAME=$1_wordpress \
   -d maschine_wordpress
+
 #  --publish 8081:80 \
 #  -p 8082:80 \
   # --publish 8081:80 --publish 80:80 \
