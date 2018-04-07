bfa822c305ecb141918348b7a7bdff7e3f74799f
Max Wittmann <damxam@gmail.com>
2018-04-07 13:41:24 +1000

    Add Dockerfile to create our nginx

--- docker-nginx.sh
+++ docker-nginx.sh
@@ -1,5 +1,5 @@
 docker run --name nginx --network backend-net --publish 80:80 -d \
-  -v $PWD/nginx.conf:/etc/nginx/nginx.conf:ro \
+  -v $PWD/max-nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
   nginx
 
 # docker run --name nginx --network backend-net --publish 80:80 -d \
